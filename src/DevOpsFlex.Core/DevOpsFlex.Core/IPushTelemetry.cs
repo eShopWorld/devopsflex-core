cb93c1ce02aa9d9b1f43de6fff713c66fb6ee609
djfr <davidferreirarodrigues@gmail.com>
2017-03-09 11:21:25 +0000

    Initial draft add of the connector contracts

---
+++ src/DevOpsFlex.Core/DevOpsFlex.Core/IPushTelemetry.cs
@@ -0,0 +1,18 @@
+﻿namespace DevOpsFlex.Core
+{
+    using System;
+
+    /// <summary>
+    /// Used by connectors that wish to push telemetry onto the telemetry platform.
+    /// </summary>
+    public interface IPushTelemetry
+    {
+        /// <summary>
+        /// Connects external packages that were loaded before big brother was loaded.
+        ///     This is used by packages loaded before big brother, so that big brother can discover them and connect
+        ///     them to start receiving their internal events.
+        /// </summary>
+        /// <param name="stream">The <see cref="IObserver{BbEvent}"/> stream that the internal package should push to.</param>
+        void Connect(IObserver<BbEvent> stream);
+    }
+}
