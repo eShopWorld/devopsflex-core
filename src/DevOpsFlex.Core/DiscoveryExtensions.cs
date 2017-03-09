097c5a98d8bd6a02e8d18812a6fd2178c96012cd
djfr <davidferreirarodrigues@gmail.com>
2017-03-09 14:21:10 +0000

    Added initial Discovery draft and a blank test project

---
+++ src/DevOpsFlex.Core/DiscoveryExtensions.cs
@@ -0,0 +1,39 @@
+﻿namespace DevOpsFlex.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using JetBrains.Annotations;
+
+    /// <summary>
+    /// Constains extensions to reflection constructs to help with discovery across the <see cref="AppDomain"/>.
+    /// </summary>
+    public static class DiscoveryExtensions
+    {
+        /// <summary>
+        /// RACING DISCOVERY CONDITIONS !!!
+        /// </summary>
+        internal static readonly object Gate = new object();
+
+        [NotNull]
+        public static IEnumerable<IPushTelemetry> GetPushConnectors([NotNull]this AppDomain domain)
+        {
+            return domain.GetConnectors<IPushTelemetry>();
+        }
+
+        [NotNull]
+        public static IEnumerable<IPullTelemetry> GetPullConnectors([NotNull]this AppDomain domain)
+        {
+            return domain.GetConnectors<IPullTelemetry>();
+        }
+
+        [NotNull]
+        internal static IEnumerable<T> GetConnectors<T>([NotNull]this AppDomain domain)
+        {
+            return domain.GetAssemblies()
+                         .SelectMany(a => a.GetTypes())
+                         .Where(typeof(T).IsAssignableFrom)
+                         .Select(t => Activator.CreateInstance<T>());
+        }
+    }
+}
---
+++ src/Tests/DevOpsFlex.Core.Tests/DiscoveryExtensionsTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevOpsFlex.Core.Tests
+{
+    [TestClass]
+    public class DiscoveryExtensionsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
