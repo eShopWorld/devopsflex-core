712eaaa01837e2b578531e0a65384f963a5024b1
djfr <davidferreirarodrigues@gmail.com>
2017-03-21 15:59:10 +0000

    Package bump to the latest

--- src/Tests/DevOpsFlex.Core.Tests/LittleBrotherPushConnectorTest.cs
+++ src/Tests/DevOpsFlex.Core.Tests/LittleBrotherPushConnectorTest.cs
@@ -8,27 +8,30 @@ using Xunit;
 // ReSharper disable once CheckNamespace
 public class LittleBrotherPushConnectorTest
 {
-    [Fact, IsUnit]
-    public void Test_Connect()
-    {
-        var resetEvent = new ManualResetEvent(false);
-        var received = false;
-        var lb = new LittleBrother();
-        var testEvent = new TestEvent();
+    //public class Connect
+    //{
+        [Fact, IsUnit]
+        public void Test_Connect()
+        {
+            var resetEvent = new ManualResetEvent(false);
+            var received = false;
+            var lb = new LittleBrother();
+            var testEvent = new TestEvent();
 
-        new TestLittleBrotherConnector().Connect().Subscribe(
-            e =>
-            {
-                e.Should().Be(testEvent);
-                received = true;
-                resetEvent.Set();
-            });
+            new TestLittleBrotherConnector().Connect().Subscribe(
+                e =>
+                {
+                    e.Should().Be(testEvent);
+                    received = true;
+                    resetEvent.Set();
+                });
 
-        lb.Publish(testEvent);
+            lb.Publish(testEvent);
 
-        resetEvent.WaitOne(TimeSpan.FromSeconds(5));
-        received.Should().BeTrue();
-    }
+            resetEvent.WaitOne(TimeSpan.FromSeconds(5));
+            received.Should().BeTrue();
+        }
+    //}
 }
 
 public class TestLittleBrotherConnector : LittleBrotherPushConnector
