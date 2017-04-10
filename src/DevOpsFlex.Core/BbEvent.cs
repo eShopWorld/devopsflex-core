4b5a586cf5ad91857135bd0b519aaf065015e97f
djfr <davidferreirarodrigues@gmail.com>
2017-04-10 13:00:00 +0100

    Pulled base conversion logic onto the Core package from Telemetry

--- src/DevOpsFlex.Core/BbEvent.cs
+++ src/DevOpsFlex.Core/BbEvent.cs
@@ -1,5 +1,9 @@
 ﻿namespace DevOpsFlex.Core
 {
+    using System.Collections.Generic;
+    using JetBrains.Annotations;
+    using Newtonsoft.Json;
+
     /// <summary>
     /// Base class for all events pushed by the telemetry platform. Can't be inherited directly but is base
     /// to all the classes that you can inherit from.
@@ -9,11 +13,24 @@
         /// <summary>
         /// Stores the internal correlation vector.
         /// </summary>
-        internal string CorrelationVector { get; set; }
+        [CanBeNull]
+        public string CorrelationVector { get; set; }
 
         /// <summary>
-        /// Internally initializes an instance of <see cref="BbEvent"/>.
+        /// Initializes an instance of <see cref="BbEvent"/>.
         /// </summary>
-        internal BbEvent() { }
+        internal BbEvent()
+        {
+        }
+
+        /// <summary>
+        /// Converts this POCO to a <see cref="IDictionary{String, String}"/> by using JSonConvert twice (both directions).
+        /// </summary>
+        /// <returns>The converted <see cref="IDictionary{String, String}"/>.</returns>
+        [NotNull]
+        public IDictionary<string, string> ToStringDictionary()
+        {
+            return JsonConvert.DeserializeObject<Dictionary<string, string>>(JsonConvert.SerializeObject(this));
+        }
     }
 }
