8a216b5e91b790b21387711016dc04f3e732ab73
djfr <davidferreirarodrigues@gmail.com>
2017-04-10 13:10:01 +0100

    Added a test for the new convertion logic in BbEvent

---
+++ src/Tests/DevOpsFlex.Core.Tests/BbEventTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using DevOpsFlex.Core;
+using DevOpsFlex.Tests.Core;
+using FluentAssertions;
+using Newtonsoft.Json;
+using Xunit;
+
+// ReSharper disable once CheckNamespace
+public class BbEventTest
+{
+    public class ToStringDictionary
+    {
+        [Fact, IsUnit]
+        public void Test_ConvertsTestPoco()
+        {
+            var poco = new TestEvent
+            {
+                SomeInt = new Random().Next(100),
+                SomeString = Lorem.GetSentence()
+            };
+
+            var result = poco.ToStringDictionary();
+
+            result[nameof(TestEvent.SomeInt)].Should().Be(poco.SomeInt.ToString());
+            result[nameof(TestEvent.SomeString)].Should().Be(poco.SomeString);
+            result.ContainsKey(nameof(TestEvent.BadReference)).Should().BeFalse();
+        }
+    }
+
+    private class TestEvent : BbEvent
+    {
+        public int SomeInt { get; set; }
+
+        public string SomeString { get; set; }
+
+        [JsonIgnore]
+        public TestEvent BadReference => this;
+    }
+}
