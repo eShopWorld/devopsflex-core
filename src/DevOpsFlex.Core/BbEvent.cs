7695dad9f7871ba84216528a1e688ba5e0335195
djfr <davidferreirarodrigues@gmail.com>
2017-04-13 13:14:22 +0100

    Final polish before adding the dependencies from the other packages

--- src/DevOpsFlex.Core/BbEvent.cs
+++ src/DevOpsFlex.Core/BbEvent.cs
@@ -1,5 +1,9 @@
 ﻿namespace DevOpsFlex.Core
 {
+    using System.Collections.Generic;
+    using JetBrains.Annotations;
+    using Newtonsoft.Json;
+
     /// <summary>
     /// Base class for all events pushed by the telemetry platform. Can't be inherited directly but is base
     /// to all the classes that you can inherit from.
@@ -14,6 +18,34 @@
         /// </remarks>
         internal BbEvent()
         {
+        }
+
+        /// <summary>
+        /// Converts this POCO to a <see cref="IDictionary{TKey,TValue}"/> by using JSonConvert twice (both directions).
+        /// </summary>
+        /// <returns>The converted <see cref="IDictionary{String, String}"/>.</returns>
+        [NotNull]
+        public IDictionary<string, string> ToStringDictionary()
+        {
+            return JsonConvert.DeserializeObject<Dictionary<string, string>>(JsonConvert.SerializeObject(this));
+        }
+
+        /// <summary>
+        /// Copies all properties on this <see cref="BbEvent"/> event to a specific target <see cref="IDictionary{String, String}"/>.
+        /// </summary>
+        /// <param name="target">The target <see cref="IDictionary{String, String}"/>.</param>
+        /// <param name="replace">true if we want to replace previously existing keys, false otherwise. If false and the key already exists, this method will throw.</param>
+        public void CopyPropertiesInto(IDictionary<string, string> target, bool replace = true)
+        {
+            var properties = ToStringDictionary();
+
+            foreach (var key in properties.Keys)
+            {
+                if (replace)
+                    target[key] = properties[key];
+                else
+                    target.Add(key, properties[key]);
+            }
         }
     }
 }
--- src/DevOpsFlex.Core/BbTelemetryEvent.cs
+++ src/DevOpsFlex.Core/BbTelemetryEvent.cs
@@ -1,8 +1,6 @@
 ﻿namespace DevOpsFlex.Core
 {
-    using System.Collections.Generic;
     using JetBrains.Annotations;
-    using Newtonsoft.Json;
 
     /// <summary>
     /// The base class from all BigBrother telemetry based events that are going to be
@@ -15,15 +13,5 @@
         /// </summary>
         [CanBeNull]
         public string CorrelationVector { get; set; }
-
-        /// <summary>
-        /// Converts this POCO to a <see cref="IDictionary{String, String}"/> by using JSonConvert twice (both directions).
-        /// </summary>
-        /// <returns>The converted <see cref="IDictionary{String, String}"/>.</returns>
-        [NotNull]
-        public IDictionary<string, string> ToStringDictionary()
-        {
-            return JsonConvert.DeserializeObject<Dictionary<string, string>>(JsonConvert.SerializeObject(this));
-        }
     }
 }
