a085a11803acbed666a4a4efa869ea2f74c2d2c9
djfr <davidferreirarodrigues@gmail.com>
2017-04-14 14:22:37 +0100

    Draft for the structure of Metrics

--- src/DevOpsFlex.Core/BbMetricEvent.cs
+++ src/DevOpsFlex.Core/BbMetricEvent.cs
@@ -6,5 +6,14 @@
     /// </summary>
     public class BbMetricEvent : BbTelemetryEvent
     {
+        /// <summary>
+        /// Gets and sets the name of the metric being pushed.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets and sets the value for the metric being pushed.
+        /// </summary>
+        public double Value { get; set; }
     }
 }
