407d5c0ebb823d72b9fe89e1d0cce8b88d76f91e
djfr <davidferreirarodrigues@gmail.com>
2017-04-18 13:45:19 +0100

    Added timed event tests

---
+++ src/Tests/DevOpsFlex.Core.Tests/BbTimeEventTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Fakes;
+using DevOpsFlex.Core;
+using DevOpsFlex.Tests.Core;
+using FluentAssertions;
+using Microsoft.QualityTools.Testing.Fakes;
+using Xunit;
+
+// ReSharper disable once CheckNamespace
+public class BbTimeEventTest
+{
+    [Fact, IsUnit]
+    public void Test_StartTime()
+    {
+        using (ShimsContext.Create())
+        {
+            var now = DateTime.Now; // freeze time
+            ShimDateTime.NowGet = () => now;
+
+            var tEvent = new BbTimedEvent();
+
+            tEvent.StartTime.Should().Be(now);
+        }
+    }
+
+    [Fact, IsUnit]
+    public void Test_EndTime()
+    {
+        using (ShimsContext.Create())
+        {
+            var now = DateTime.Now; // freeze time
+            ShimDateTime.NowGet = () => now;
+
+            var tEvent = new BbTimedEvent();
+            tEvent.End();
+
+            tEvent.EndTime.Should().Be(now);
+        }
+    }
+}
