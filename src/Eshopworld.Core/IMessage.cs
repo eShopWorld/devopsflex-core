e8de7b41600c033e3b8c41b7196de4d7d65b34c6
djfr <davidferreirarodrigues@gmail.com>
2018-02-24 13:22:44 +0000

    Added contracts to messaging and telemetry to core instead of edge packages

---
+++ src/Eshopworld.Core/IMessage.cs
@@ -0,0 +1,7 @@
+﻿namespace Eshopworld.Core
+{
+    /// <summary>
+    /// Marks a class as a message to be serialized and deserialized from the body of a Service Bus message.
+    /// </summary>
+    public interface IMessage { }
+}
