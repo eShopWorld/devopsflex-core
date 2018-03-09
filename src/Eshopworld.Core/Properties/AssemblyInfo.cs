c694ebd85caf8a05c893bf1b14539156d430ec6b
djfr <davidferreirarodrigues@gmail.com>
2018-03-09 16:41:29 +0000

    Fixed moq visibility

--- src/Eshopworld.Core/Properties/AssemblyInfo.cs
+++ src/Eshopworld.Core/Properties/AssemblyInfo.cs
@@ -1,5 +1,7 @@
 ﻿using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 [assembly: InternalsVisibleTo("Eshopworld.Core.Tests")]
+
 [assembly: InternalsVisibleTo("Eshopworld.Telemetry")]
 [assembly: InternalsVisibleTo("Eshopworld.Messaging")]
