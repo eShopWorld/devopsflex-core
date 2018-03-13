f962fdae74f08d118fad75b812835d329d4f1244
djfr <davidferreirarodrigues@gmail.com>
2018-03-13 11:14:36 +0000

    Added test projects as friendly assemblies

--- src/Eshopworld.Core/Properties/AssemblyInfo.cs
+++ src/Eshopworld.Core/Properties/AssemblyInfo.cs
@@ -4,4 +4,7 @@
 [assembly: InternalsVisibleTo("Eshopworld.Core.Tests")]
 
 [assembly: InternalsVisibleTo("Eshopworld.Telemetry")]
+[assembly: InternalsVisibleTo("Eshopworld.Telemetry.Tests")]
+
 [assembly: InternalsVisibleTo("Eshopworld.Messaging")]
+[assembly: InternalsVisibleTo("Eshopworld.Messaging.Tests")]
