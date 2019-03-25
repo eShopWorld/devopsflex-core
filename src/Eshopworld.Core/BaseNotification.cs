3686003065e49d8d7700dd7a09e168b83c675a8c
Vaclav Haken <vaclav@vaclavkabat.net>
2019-03-25 17:01:59 +0000

    KV changed event introduced along with base type and version bumped up @ major version level

---
+++ src/Eshopworld.Core/BaseNotification.cs
@@ -0,0 +1,14 @@
+﻿namespace Eshopworld.Core
+{
+    /// <summary>
+    /// base type for all notifications
+    /// </summary>
+    public class BaseNotification
+    {
+        /// <summary>
+        /// non public constructor to prevent instantiation at this level
+        /// </summary>
+        protected BaseNotification()
+        { }
+    }
+}
