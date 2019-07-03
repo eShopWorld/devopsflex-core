a156484b04932d4d86995b2512791ad9d6892f0d
Vaclav Haken <vaclav@vaclavkabat.net>
2019-07-03 13:16:54 +0100

    interface defined

---
+++ src/Eshopworld.Core/IDNSConfigurationCascade.cs
@@ -0,0 +1,15 @@
+﻿namespace Eshopworld.Core
+{
+    /// <summary>
+    /// this interface defines evolution platform (V2) service (e.g. API) deployment cascade and possible routes to its instances
+    ///
+    /// Proxy (Service Fabric Reverse Proxy) -> Cluster (regional external load balancer) -> Global (FrontDoor)
+    /// </summary>
+    // ReSharper disable once InconsistentNaming
+    public interface IDNSConfigurationCascade
+    {
+        string Proxy { get; set; }
+        string Cluster { get; set; }
+        string Global { get; set; }
+    }
+}
