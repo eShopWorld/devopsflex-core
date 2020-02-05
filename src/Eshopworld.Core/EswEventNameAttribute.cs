b35c0e454aed835b22ec7f6855ce825556a53068
Stephen Poland <spoland@eshopworld.com>
2020-02-05 16:49:57 +0000

    Add EswEventNameAttribute
    This commit adds the EswEventNameAttribute and tests.

---
+++ src/Eshopworld.Core/EswEventNameAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Eshopworld.Core
+{
+    /// <summary>
+    /// Describes an Esw Event Name Attribute
+    /// </summary>
+    /// <seealso cref="System.Attribute" />
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public sealed class EswEventNameAttribute : Attribute
+    {
+        /// <summary>
+        /// Gets or sets the name of the event.
+        /// </summary>
+        public string EventName { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EswEventNameAttribute"/> class.
+        /// </summary>
+        /// <param name="eventName">Name of the event.</param>
+        public EswEventNameAttribute(string eventName)
+        {
+            EventName = eventName;
+        }
+    }
+}
