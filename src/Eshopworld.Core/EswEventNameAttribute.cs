93a63562277bfe70db0510766777dededcf55861
Stephen Poland <stephen.poland@outlook.com>
2020-02-14 09:35:23 +0000

    Update XML comments.

--- src/Eshopworld.Core/EswEventNameAttribute.cs
+++ src/Eshopworld.Core/EswEventNameAttribute.cs
@@ -3,21 +3,23 @@
 namespace Eshopworld.Core
 {
     /// <summary>
-    /// Describes an Esw Event Name Attribute
+    /// Describes an Esw Event Name Attribute.
+    /// Should be used to override the default event name that Captain Hook includes when 
+    /// calling an endpoint in response to an event.
     /// </summary>
     /// <seealso cref="System.Attribute" />
     [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
     public sealed class EswEventNameAttribute : Attribute
     {
         /// <summary>
-        /// Gets or sets the name of the event.
+        /// Fully qualified event name
         /// </summary>
         public string EventName { get; set; }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="EswEventNameAttribute"/> class.
         /// </summary>
-        /// <param name="eventName">Name of the event.</param>
+        /// <param name="eventName">The Fully qualified event name that captain hook should include when calling an endpoint.</param>
         public EswEventNameAttribute(string eventName)
         {
             EventName = eventName;
